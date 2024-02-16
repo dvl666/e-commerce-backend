d3eb34e67e741ae7d3506e33bfbad6b40f2d08e5
dvl666 <kamagaspar@gmail.com>
2024-02-16 15:49:30 -0300

    update(auth.service): update

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,23 +1,25 @@
-import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
+import {  Injectable, UnauthorizedException, UseGuards } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { SingInDto } from './dto/signin.dto';
 import { RegisterDto } from './dto/register.dto';
 import * as bcryptjs from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
 
     constructor(
-        private readonly usersService: UsersService
+        private readonly usersService: UsersService,
+        private readonly jwtService: JwtService
     ) {}
 
-    async signIn({ email, password }: SingInDto ) {
+    async signIn({ email, password }: SingInDto ): Promise<{ access_token: string }> {
         const user = await this.usersService.findOneByEmailWithPassword(email);
         const isValidPassword = await bcryptjs.compare(password, user.password)
         if(!isValidPassword) throw new UnauthorizedException('Password is wrong')
-        // if(user.password !== password) throw new BadRequestException('password is wrong')
+        const payload = { email: user.email, role: user.role }
         return {
-            user
+            access_token: await this.jwtService.signAsync(payload)
         }   
     }
 
