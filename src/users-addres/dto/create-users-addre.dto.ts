2197995dcc82220adbef9a9cf3197a12a755bf38
dvl666 <kamagaspar@gmail.com>
2024-02-17 16:28:21 -0300

    feat(users-addres.dto)

--- src/users-addres/dto/create-users-addre.dto.ts
+++ src/users-addres/dto/create-users-addre.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUsersAddreDto {}
+import { IsNumber, IsOptional, IsString } from "class-validator"
+
+export class CreateUsersAddreDto {
+
+    @IsString()
+    region: string
+
+    @IsString()
+    comune: string
+    
+    @IsString()
+    addres: string
+
+    @IsNumber()
+    phoneNumber: string
+
+    @IsString()
+    @IsOptional()
+    userEmail?: string
+
+}
