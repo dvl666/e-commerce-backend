42287745e4f2b1dcbebd3f45d15cc4f9bc4983c0
dvl666 <kamagaspar@gmail.com>
2024-02-17 16:28:12 -0300

    feat(users-addres.entity)

--- src/users-addres/entities/users-addre.entity.ts
+++ src/users-addres/entities/users-addre.entity.ts
@@ -1,5 +1,32 @@
+import { User } from "src/users/entities/user.entity";
+import { Column, DeleteDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
 export class UsersAddre {
 
-    
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    region: string
+
+    @Column()
+    comune: string
+
+    @Column()
+    addres: string
+
+    @Column()
+    phoneNumber: string
+
+    @ManyToOne(() => User)
+    @JoinColumn({ name: 'userEmail', referencedColumnName: 'email', })
+    user: User;
+
+    @Column()
+    userEmail: string;
+
+    @DeleteDateColumn()
+    deletedAt: Date
 
 }
