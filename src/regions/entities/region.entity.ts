0c3b6468653382967686998e3ce85cb22b9741af
dvl666 <kamagaspar@gmail.com>
2024-02-18 21:58:53 -0600

    update(regions.entity):

--- src/regions/entities/region.entity.ts
+++ src/regions/entities/region.entity.ts
@@ -1,1 +1,12 @@
-export class Region {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Region {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    name: string
+
+}
