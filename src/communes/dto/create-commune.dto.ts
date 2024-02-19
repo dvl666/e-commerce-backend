6130b56845ee5818597cc0261a50f76981cd8dda
dvl666 <kamagaspar@gmail.com>
2024-02-19 14:57:20 -0600

    update(communes.: update create DTO

--- src/communes/dto/create-commune.dto.ts
+++ src/communes/dto/create-commune.dto.ts
@@ -2,6 +2,7 @@ import { IsInt, IsString } from "class-validator";
 import { OneToMany } from "typeorm";
 import { Commune } from "../entities/commune.entity";
 import { Region } from "src/regions/entities/region.entity";
+import { Province } from "src/provinces/entities/province.entity";
 
 export class CreateCommuneDto {
 
@@ -9,6 +10,6 @@ export class CreateCommuneDto {
     name: string
     
     @IsInt()
-    region: Region
+    province: Province
 
 }
