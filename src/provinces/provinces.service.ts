7d6085b129e188a5578583b2a3e2239c45ff97f8
dvl666 <kamagaspar@gmail.com>
2024-02-20 13:28:33 -0600

    update(province.service): add findOneWithCommunes

--- src/provinces/provinces.service.ts
+++ src/provinces/provinces.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateProvinceDto } from './dto/create-province.dto';
 import { UpdateProvinceDto } from './dto/update-province.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Province } from './entities/province.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProvincesService {
+
+  constructor(
+    @InjectRepository(Province)
+    private readonly provinceRepository: Repository<Province>
+  ) {}
+
   create(createProvinceDto: CreateProvinceDto) {
     return 'This action adds a new province';
   }
@@ -12,8 +21,10 @@ export class ProvincesService {
     return `This action returns all provinces`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} province`;
+  async findOne(id: number) {
+    const province = await this.provinceRepository.findOneBy({ id: id });
+    if(!province) throw new BadRequestException('Province not found');
+    return province
   }
 
   update(id: number, updateProvinceDto: UpdateProvinceDto) {
@@ -22,5 +33,14 @@ export class ProvincesService {
 
   remove(id: number) {
     return `This action removes a #${id} province`;
+  }
+
+  async findOneWithCommunes(id: number) {
+    await this.findOne(id)
+    const province = await this.provinceRepository.createQueryBuilder("province")
+      .leftJoinAndSelect("province.communes", "commune")
+      .where("province.id = :id", { id: id })
+      .getOne()
+    return province
   }
 }
