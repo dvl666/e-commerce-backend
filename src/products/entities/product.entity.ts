3ff392a46bc6d07d27db802062149efbe7e1e932
dvl666 <kamagaspar@gmail.com>
2024-02-24 18:09:01 -0300

    update(create.-products.dto): update

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,21 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Product {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column({ nullable: false })
+    name: string
+
+    @Column()
+    description: string
+
+    @Column({ nullable: false })
+    price: number
+
+    @Column({ default: 'not implement' })
+    category: string
+
+}
