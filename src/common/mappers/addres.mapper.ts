97ef235a933416cd1cb975562c07fcd4def6fa1b
dvl666 <kamagaspar@gmail.com>
2024-02-23 23:37:02 -0300

    add(addres.mapper)

---
+++ src/common/mappers/addres.mapper.ts
@@ -0,0 +1,39 @@
+import { Addre } from "src/addres/entities/addre.entity";
+
+export function mapAddressResults(addresses: Addre[]) {
+    const uniqueProfile = addresses.length > 0 ? mapProfile(addresses[0].profile) : null;
+  
+    const mappedAddresses = addresses.map(address => ({
+      id: address.id,
+      addres: address.addres,
+      postalCode: address.postalCode,
+      commune: address.commune,
+      userEmail: address.profile?.user?.email,
+    }));
+  
+    return [
+      {
+        profile: uniqueProfile,
+      },
+      ...mappedAddresses,
+    ];
+  }
+  
+  function mapProfile(profile) {
+    return {
+      id: profile.id,
+      name: profile.name,
+      lastName: profile.lastName,
+      rut: profile.rut,
+      user: mapUser(profile.user),
+    };
+  }
+  
+  function mapUser(user) {
+    return {
+      id: user.id,
+      email: user.email,
+      role: user.role,
+      deletedAT: user.deletedAT,
+    };
+  }
