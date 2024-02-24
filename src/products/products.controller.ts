e78dc692e689d1c2f2ed33a419fd6068a42ad7b4
dvl666 <kamagaspar@gmail.com>
2024-02-24 18:07:54 -0300

    update(products.controller): update

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -2,33 +2,55 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { Role } from 'src/common/enums/role.enum';
+import { ActiveUser } from 'src/common/decorators/active.user.decorator';
+import { UserActiveInterface } from 'src/common/interfaces/user.active.interface';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
+  @Auth(Role.ADMIN)
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productsService.create(createProductDto);
+  create(
+      @Body() createProductDto: CreateProductDto,
+      @ActiveUser() user: UserActiveInterface
+    ) {
+    return this.productsService.create(createProductDto, user);
   }
 
+  // @Auth(Role.USER)
   @Get()
   findAll() {
     return this.productsService.findAll();
   }
 
+  // @Auth(Role.USER)
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(
+      @Param('id') id: string,
+      // @ActiveUser() user: UserActiveInterface  
+    ) {
     return this.productsService.findOne(+id);
   }
 
+  @Auth(Role.ADMIN)
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
+  update(
+      @Param('id') id: string, 
+      @Body() updateProductDto: UpdateProductDto,
+      @ActiveUser() user: UserActiveInterface
+    ) {
+    return this.productsService.update(+id, updateProductDto, user);
   }
 
+  @Auth(Role.ADMIN)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productsService.remove(+id);
+  remove(
+      @Param('id') id: string,
+      @ActiveUser() user: UserActiveInterface
+    ) {
+    return this.productsService.remove(+id, user);
   }
 }
